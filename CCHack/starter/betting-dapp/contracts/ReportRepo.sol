ca3a6fd8a94ca48b42ec55f9ede38b4faa630cac
krtonga <kristen.tonga@gmail.com>
2018-04-07 17:13:02 -0400

    Update ReportRepo to create wallet in constructor

--- CCHack/starter/betting-dapp/contracts/ReportRepo.sol
+++ CCHack/starter/betting-dapp/contracts/ReportRepo.sol
@@ -4,54 +4,56 @@ import "./escrowSimple.sol";
 import "./factory.sol";
 import "./ModelWallet.sol";
 
-contract ReportRepo is Factory{
+contract ReportRepo is Factory {
 
   address user;
   address HRrep;
-  
+
   // enum capture for Escrow state?
-  
+
   User[] users;
   Message[] messages;
-  
+  address walletAddress;
+
   struct User {
-        address owner; /* This is an Ethereum Account Address - user must have keys and password */
-        address walletAddress;
+    address owner; /* This is an Ethereum Account Address - user must have keys and password */
+    address walletAddress;
   }
 
   struct Message{
-        address owner;
-        string message;
-        uint32 attachIPFS;
+    address owner;
+    string message;
+    uint32 attachIPFS;
   }
 
-  function createReportWallet(address[] _users, uint _required) public returns (address wallet) {
-    wallet = new ModelWallet(_users, _required);
-    register(wallet);
+  function ReportRepo(address[] _users, uint _required) public {
+    // Generate the multi-sig wallet
+    walletAddress = new ModelWallet(_users, _required);
+    register(walletAddress);
   }
-  
+
+  function getWalletAddress() public view returns (address) {
+      return walletAddress;
+  }
+
   function addMessage(address _sender, string _message, uint32 _attachIPFS) public {
         Message memory newMsg;
-        
+
         _sender == msg.sender;
-        newMsg.owner = _sender;  
+        newMsg.owner = _sender;
         newMsg.message = _message;
         newMsg.attachIPFS = _attachIPFS;
         messages.push(newMsg);
-        
+
    }
-      
+
    //function send funds to Escrow
-   
-   //modifiers to change states for Escrow 
-   
+
+   //modifiers to change states for Escrow
+
    //return funds from Escrow
-   
+
    //modifier to change state of contract from in progress, under investigation, resolved, in dispute
-   
-   
+
+
 }
-
-
-
-
